42c755da50f526ce7ad654fe764d2fedbf55a442
Lucas Rufino <lucaslrt@outlook.com>
2017-04-14 13:26:06 -0300

    Adding 2 players.

--- src/JogoDaVelha.hs
+++ src/JogoDaVelha.hs
@@ -19,23 +19,34 @@ data Tabuleiro = Tabuleiro Trio Trio Trio -- O tabuleiro eh feito de tres linhas
 instance Show Tabuleiro where -- Show eh uma classe de Data.Maybe
     show tabuleiro =
         -- unlines transforma um vetor de strings em uma unica string, acrescentanto \n
-        unlines . surround "+---+---+---+" . map (concat . surround "|". map showSquare) $ (linhas tabuleiro)
+        unlines . surround "+---+---+---+" . map (concat . surround "|". map showBloco) $ (linhas tabuleiro)
         where
         surround x xs = [x] ++ intersperse x xs ++ [x] -- intersperse acrescenta um elemento x entre os elementos de xs
-        showSquare = either (\n -> " " ++ show n ++ " ") (concat . replicate 3 . show)
+        showBloco = either (\n -> " " ++ show n ++ " ") (concat . replicate 3 . show)
 
 -- Linhas do Tabuleiro
 linhas :: Tabuleiro -> [Trio]
 linhas (Tabuleiro x y z) = [x, y, z]
 
 stop :: Int -> String
-stop a = concat ["\ESC[", show a, "m"] -- Interrompe a inserçao de cor
+stop n = concat ["\ESC[", show n, "m"] -- Interrompe a inserçao de cor
 
-color :: Simbolo -> String
-color s
+cor :: Simbolo -> String
+cor s
         | s == X     = stop 36++" "++show s++" "++stop 0 -- 36 eh a cor ciano
         | s == O     = stop 32++" "++show s++" "++stop 0 -- 32 eh a cor verde
 
 -- |Convenience function for constructing an empty board
 tabuleiroVazio :: Tabuleiro
 tabuleiroVazio = Tabuleiro [Left 1, Left 2, Left 3] [Left 4, Left 5, Left 6] [Left 7, Left 8, Left 9]
+
+-- |Given either a row, column, or diagonal, it checks whether it is entirely filled with naughts or crosses
+trioCheio :: Trio -> Bool
+trioCheio ts@[a,b,c] = noLefts && allEqual
+	where
+		noLefts = foldl (\acc curr -> acc && (isRight curr)) True ts
+		allEqual = a == b && b == c
+
+-- |Given a game board, check whether the game is over because someone won
+ganhou :: Tabuleiro -> Bool
+ganhou t = foldl (\acc curr -> acc || (trioCheio curr)) False ((linhas t) ++ (colunas t) ++ (diagonais t))
--- src/Main.hs
+++ src/Main.hs
@@ -6,3 +6,40 @@ import System.IO
 main :: IO ()
 main = do
     putStrLn "====== Jogo da Velha ======"
+
+    -- |Grab the user's move, and feed that to tic-tac-toe. Recurse as needed.
+
+-- Funçao para auxiliar que o prompt vai ser feito antes do usuario dar um input
+promptLine :: String -> IO String
+promptLine text = do
+    putStr text
+    hFlush stdout
+    getLine
+
+-- Mostra o fim do jogo e o ganhador
+fimDeJogo :: Tabuleiro -> IO ()
+fimDeJogo t = do
+	putStrLn (show t)
+	putStrLn (winner t)
+
+jogador1 :: Tabuleiro -> IO ()
+jogador1 tabuleiro = do
+	putStrLn (show tabuleiro)
+	posicao <- promptLine "Turno do jogador 1: \nEscolha um bloco para jogar "
+	putStrLn ""
+	let selecPosicao = Left (read posicao) -- Pega o input do jogador e adiciona X nela
+	let novoTab = findAndReplace tabuleiro selecPosicao (Right X)
+	if ganhou novoTab || empate novoTab
+		then fimDeJogo novoTab
+		else jogador2 novoTab
+
+jogador2 :: Tabuleiro -> IO ()
+jogador2 tabuleiro = do
+	putStrLn (show tabuleiro)
+	posicao <- promptLine "Turno do jogador 2: \nEscolha um bloco para jogar "
+	putStrLn ""
+	let selecPosicao = Right (read loc) -- Pega o input do jogador e adiciona O nela
+	let novoBoard = findAndReplace tabuleiro selecPosicao (Left O)
+	if ganhou novoTab || empate novoTab
+		then fimDeJogo novoTab
+		else jogador1 novoTab
